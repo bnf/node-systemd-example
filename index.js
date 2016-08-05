8ad43dc80086940e04afdd4935d6ef5e36e21b54
Benjamin Franzke <benjaminfranzke@gmail.com>
2016-08-05 23:24:04 +0200

    Implement graceful shutdown on SIGHUP

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const http = require('http');
 const sd = require('systemd-daemon');
+const hgc = require('./lib/http-graceful-close');
 
 const handle = sd.socket() || 3000;
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
+	setTimeout(() => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Hello World\n');
+	}, 5000);
+});
+
+hgc.prepare(server);
+
+process.on('SIGHUP', function() {
+	sd.notify("STOPPING=1");
+	/* 111 is convention between this process and the systemd service.
+	 * .. trigger systemd to restart the process. */
+	process.exitCode = 111;
+	hgc.close(server);
 });
 
 server.listen(handle, () => {
