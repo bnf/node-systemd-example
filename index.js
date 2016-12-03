bbb79d6cd020d6a29490683d50265027a311a90c
Benjamin Franzke <benjaminfranzke@gmail.com>
2016-12-03 07:57:31 +0100

    Add watchdog and exit-on-idle support and rework the graceful close lib
    Oh and also add the service files

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const http = require('http');
 const sd = require('systemd-daemon');
 const hgc = require('./lib/http-graceful-close');
+const eoi = require('./lib/exit-on-idle');
+
+sd.watchdog.start();
 
 const handle = sd.socket() || 3000;
 
 const server = http.createServer((req, res) => {
-	setTimeout(() => {
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain');
-		res.end('Hello World\n');
-	}, 5000);
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World\n');
 });
 
-hgc.inject(server);
+const close = (exitCode) => {
+	return () => {
+		process.exitCode = exitCode ? exitCode : 0;
+		sd.notify("STOPPING=1");
+		server.emit('http-graceful-close');
+		server.close(() => {
+			sd.watchdog.stop();
+		});
+	};
+};
 
-process.on('SIGHUP', function() {
-	sd.notify("STOPPING=1");
-	/* 111 is convention between this process and the systemd service.
-	 * .. trigger systemd to restart the process. */
-	process.exitCode = 111;
-	server.close();
-});
+server.on('connection', hgc.onConnection);
+server.on('connection', eoi.onIdle(close(), 30000));
 
 server.listen(handle, () => {
 	sd.notify("READY=1");
+	/* 111 is convention between this process and the systemd service.
+	 * .. trigger systemd to restart the process. */
+	process.on('SIGHUP', close(111));
 });
